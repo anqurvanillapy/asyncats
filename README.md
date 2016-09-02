727423bd7226cf23a1c7a0087cfcf6e822b09444
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-09-02 23:02:55 +0800

    updated py-echo with runnability

--- README.md
+++ README.md
@@ -10,6 +10,27 @@ from standard library) for asynchronicity instead of third-party packages.
 language's standard library, hence some of the code will share a totally
 different style to implement an asynchronous echo server.
 
+## Example
+
+Each piece of code will share the same interaction, and listen to localhost on
+port 8080 for echoing simple message. For instance, the client side:
+
+```bash
+$ nc localhost 8080
+foo
+foo
+bar
+bar
+```
+
+The server will returns the actual message you just send. On the server side:
+
+```
+Available on localhost:8080
+Connection from ('127.0.0.1', 36440)
+('127.0.0.1', 36440) disconnected
+```
+
 ## Installation
 
 ## Usage
--- py-echo/server.py
+++ py-echo/server.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python3
 
-import socket
+"""\
+# server.py
+
+Async/await in tasks and coroutines in Python 3.5 is decent for making a
+simple message echoing server in socket programming. If more pragmatic
+third-party packages could be accepted, `curio' and `h2/h11' may be my
+prominent choices for low-level networking.
+"""
+
+from socket import *
 import asyncio
 
+async def server(addr, port):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
+    sock.bind((addr, port))
+    sock.listen(3)
+    sock.setblocking(False)     # equivalent to sock.settimeout(0.0)
+    print('Available on {0}:{1}'.format(addr, port))
+    while True:
+        conn, addr = await loop.sock_accept(sock)
+        loop.create_task(conn_handler(conn, addr))
 
-async def server():
-    print('TODO')
-    return
+
+async def conn_handler(conn, addr):
+    print('Connection from', addr)
+    with conn:
+        while True:
+            data = await loop.sock_recv(conn, 1024)
+            if not data: break
+            await loop.sock_sendall(conn, bytes(data))
+    print(addr, 'disconnected')
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(server('localhost', 8080))
+loop.run_forever()
